b467f365db53a52e2659c29408e93c2b74df0635
Nick <nick6027@gmail.com>
2017-07-10 21:06:27 -0400

    Added main menu page for creating ride or drive

--- app/src/main/java/softwareengineering/uc_carpooling/MainMenuActivity.java
+++ app/src/main/java/softwareengineering/uc_carpooling/MainMenuActivity.java
@@ -1,5 +1,7 @@
 package softwareengineering.uc_carpooling;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,9 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, MainMenuActivity.class);
     }
 }
