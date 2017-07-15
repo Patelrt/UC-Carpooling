bcad7dce641f7cf27ae0036181aa96d3242b7a3d
Justin Schneider <mobiusone6@gmail.com>
2017-07-15 14:13:03 -0400

    + Added central location for verifying passwords, emails, usernames, etc + Added the basics of a start thing for EditUserProfile

---
+++ app/src/main/java/softwareengineering/uc_carpooling/CommonErrorMessages.java
@@ -0,0 +1,13 @@
+package softwareengineering.uc_carpooling;
+
+/**
+ * Created by jjsch on 7/15/2017.
+ */
+
+public class CommonErrorMessages {
+
+    public static String InvalidEmail = "Email must be a valid UC email address.";
+    public static String EmptyEmail = "Must input a valid email";
+    public static String PasswordInvalid = "Error Password Invalid, must be at least 6 characters and contain at least 1 capital and lowercase letter and at least 1 number.";
+    public static String UsernameInvalid = "username must be more than 6 characters"; // and unique";
+}
--- app/src/main/java/softwareengineering/uc_carpooling/EditUserProfile.java
+++ app/src/main/java/softwareengineering/uc_carpooling/EditUserProfile.java
@@ -2,12 +2,61 @@ package softwareengineering.uc_carpooling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class EditUserProfile extends AppCompatActivity {
+
+    // stores the FirebaseAuth context
+    private FirebaseAuth mAuth;
+
+    // stores the user who is currently logged in
+    private FirebaseUser fbUser;
+
+    // stores username
+    private String username;
+
+    // stores the false password text
+    private String password;
+
+    // stores email
+    private String email;
+
+    // class that validates email and password
+    private Validation validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_profile);
+
+        //Initialize variables
+        validator = new Validation();
+        mAuth = FirebaseAuth.getInstance();
+        fbUser = mAuth.getCurrentUser();
+        username = fbUser.getDisplayName();
+        password = "******";
+        email = fbUser.getEmail();
+
+        // Initialize box text to user's information
+        ((EditText)findViewById(R.id.input_username)).setText(username);
+        ((EditText)findViewById(R.id.input_email)).setText(email);
+
+
+
+        // Override on Click Listener for the button
+        Button mUpdateButton = (Button) findViewById(R.id.editProfileSave);
+        mUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText textToValidate = ((EditText)(findViewById(R.id.input_username)));
+                validator.validateUsername(textToValidate);
+
+            }
+        });
     }
 }
