a4a036b8e30db1051a7e19065b9fed6d59646136
Justin Schneider <mobiusone6@gmail.com>
2017-07-16 21:44:26 -0400

    Fixed profile view and profile edit pages to appear and navigate between them, but you still can't actually edit the profile properties

--- app/src/main/java/softwareengineering/uc_carpooling/EditUserProfile.java
+++ app/src/main/java/softwareengineering/uc_carpooling/EditUserProfile.java
@@ -1,15 +1,23 @@
 package softwareengineering.uc_carpooling;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditUserProfile extends AppCompatActivity {
+
+    // stores Firebase database reference
+    private DatabaseReference mDatabase;
 
     // stores the FirebaseAuth context
     private FirebaseAuth mAuth;
@@ -21,7 +29,10 @@ public class EditUserProfile extends AppCompatActivity {
     private String username;
 
     // stores the false password text
-    private String password;
+    //private String password;
+
+    // stores the full name of the user
+    private String fullName;
 
     // stores email
     private String email;
@@ -36,16 +47,27 @@ public class EditUserProfile extends AppCompatActivity {
 
         //Initialize variables
         validator = new Validation();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         fbUser = mAuth.getCurrentUser();
-        username = fbUser.getDisplayName();
-        password = "******";
-        email = fbUser.getEmail();
+
+        username = "defaultusername";//(fbUser.getDisplayName().isEmpty() ? "(none)" : fbUser.getDisplayName());
+        fullName = "defaultname";
+        //password = "******";
+        email = "example@gmail.com";
+
+        // get user information
+        if (fbUser != null)
+        {
+            username = fbUser.getDisplayName();
+            email = fbUser.getEmail();
+            fullName = fbUser.getUid();
+        }
 
         // Initialize box text to user's information
         ((EditText)findViewById(R.id.input_username)).setText(username);
         ((EditText)findViewById(R.id.input_email)).setText(email);
-
+        //(TextView)findViewById(R.id.input_password).setText(password);
 
 
         // Override on Click Listener for the button
@@ -58,5 +80,9 @@ public class EditUserProfile extends AppCompatActivity {
 
             }
         });
+    }
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, EditUserProfile.class);
     }
 }
